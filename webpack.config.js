b08928a96c8142f4a4b80f73e3946476bbbd81f5
davidjj76 <davidjj76@gmail.com>
2017-04-22 01:41:26 +0200

    Create App, Header and Main components

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,10 @@ module.exports = {
         use: 'babel-loader'
       }, {
         test: /\.css$/,
-        use: `style-loader!css-loader?${cssModules}`
+        use: [
+          'style-loader',
+          `css-loader?${cssModules}`
+        ]
       }
     ]
   },
