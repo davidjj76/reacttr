be97da9b166853e4cefb35c45e0e41125ab78cd8
davidjj76 <davidjj76@gmail.com>
2017-04-23 23:04:04 +0200

    Create ProfileBar component

---
+++ src/components/ProfileBar/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import styles from './profile-bar.css'
+
+class ProfileBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <figure>
+          <img className={styles.avatar} src={this.props.picture} />
+        </figure>
+        <span className={styles.username}>Hola @{this.props.username}!</span>
+        <button onClick={this.props.onOpenText} className={styles.button}>
+          <span className="fa fa-lg fa-edit"></span>
+          Tweet!
+        </button>
+      </div>
+    )
+  }
+
+}
+
+export default ProfileBar
